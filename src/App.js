096964628610fe0b42d8aa9e4e50ddcc11d745e9
Robin Andersson <k.robin.andersson@gmail.com>
2019-06-09 18:41:51 +0200

    determine next action after answer
    Only logs the action object to console for now.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,47 @@ function App() {
     questionnaire.questions[0]
   );
   const [currentAnswer, setCurrentAnswer] = useState();
+  const [currentScore, setCurrentScore] = useState(0);
 
-  const onAnswerChange = evt => setCurrentAnswer(evt.target.value);
+  const onAnswerChange = evt =>
+    setCurrentAnswer(() =>
+      currentQuestion.answers.find(answer => answer.id === evt.target.value)
+    );
+
   const onAnswerSubmit = evt => {
     evt.preventDefault();
+
+    const { id, score } = currentAnswer;
+    setCurrentScore(currentScore + score);
+
+    // determine the type and id of the next action
+    const nextAction = (() => {
+      for (let answer of currentQuestion.next) {
+        const { next_question, answered, max_score, outcome } = answer;
+
+        const type = answer.hasOwnProperty('next_question')
+          ? 'next_question'
+          : 'outcome';
+
+        switch (type) {
+          case 'next_question':
+            if (!answer.hasOwnProperty('answered') || answered === id) {
+              return { type, id: next_question };
+            }
+            break;
+          case 'outcome':
+            if (
+              !answer.hasOwnProperty('max_score') ||
+              currentScore <= max_score
+            ) {
+              return { type, id: outcome };
+            }
+            break;
+        }
+      }
+    })();
+
+    console.log(nextAction);
   };
 
   return (
